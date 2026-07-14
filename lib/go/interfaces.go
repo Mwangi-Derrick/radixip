5d26a53df2fcc89032f8aa283af6b497de3ad6d6
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-07-14 20:58:18 +0300

    feat: create radixnode, radix engine and engine factory interfaces

--- lib/go/interfaces.go
+++ lib/go/interfaces.go
@@ -1,5 +1,7 @@
 package go
 
+import "net"
+
 // EngineVariant represents the type of engine to use
 type EngineVariant string
 
@@ -20,3 +22,37 @@ const (
 	NodePadded   NodeVariant = "padded"
 )
 
+
+//  RADIX NODE INTERFACE 
+type RadixNode interface {
+    Bit() *uint8
+    Left() RadixNode
+    Right() RadixNode
+    Metadata() *Metadata
+    Prefix() *net.IPNet
+    GetChild(network *net.IPNet) RadixNode
+    InsertChild(network *net.IPNet, node RadixNode)
+    RemoveChild(network *net.IPNet) RadixNode
+    SetMetadata(metadata Metadata)
+    ClearMetadata()
+    SetBit(bit uint8)
+    SetPrefix(prefix *net.IPNet)
+}
+
+//  RADIX ENGINE INTERFACE 
+type RadixEngine interface {
+    Insert(prefix *net.IPNet, metadata Metadata) error
+    Lookup(ip net.IP) *Metadata
+    Remove(prefix *net.IPNet) *Metadata
+    Contains(prefix *net.IPNet) bool
+    Clear()
+    Size() int
+    Stats() *EngineStats
+}
+
+
+// FACTORY INTERFACE 
+type EngineFactory interface {
+    CreateEngine(variant EngineVariant) RadixEngine
+    CreateNode(variant NodeVariant) RadixNode
+}
