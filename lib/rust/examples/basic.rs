7ecc230e7c2b02f177c1ae4dba3f2518204f4b19
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-07-14 12:55:23 +0300

    feat: basic example

---
+++ lib/rust/examples/basic.rs
@@ -0,0 +1,17 @@
+use std::net::IpAddr;
+
+use ipnetwork::IpNetwork;
+use radixip::{Metadata, NodeVariant, RadixEngine, StandardEngine};
+
+fn main() {
+    let engine = StandardEngine::new(NodeVariant::Normal);
+    engine
+        .insert(
+            "10.0.0.0/8".parse::<IpNetwork>().unwrap(),
+            Metadata::new("internal"),
+        )
+        .unwrap();
+
+    let ip = "10.1.2.3".parse::<IpAddr>().unwrap();
+    println!("{:?}", engine.lookup(&ip));
+}
