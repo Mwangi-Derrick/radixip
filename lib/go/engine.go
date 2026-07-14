26050db0ab2a8eaeafe6813852f75ef1ebf33ce2
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-07-14 09:24:34 +0300

    feat: insert with metadata

--- lib/go/engine.go
+++ lib/go/engine.go
@@ -23,3 +23,23 @@ func NewRadixEngine() *RadixEngine {
 		size: 0,
 	}
 }
+
+
+// Insert adds a subnet with metadata
+func (e *RadixEngine) Insert(subnet string, metadata Metadata) error {
+	// Parse subnet
+	_, network, err := net.ParseCIDR(subnet)
+	if err != nil {
+		return ErrInvalidSubnet
+	}
+
+	// Copy-on-write: create new tree with insert
+	oldRoot := (*radixNode)(atomic.LoadPointer(&e.root))
+	newRoot := oldRoot.cloneWithInsert(network, metadata)
+
+	// Atomic swap
+	atomic.StorePointer(&e.root, unsafe.Pointer(newRoot))
+	atomic.AddUint64(&e.size, 1)
+
+	return nil
+}
