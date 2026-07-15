950bf216099861a8d6ce91a6b13229aa4fcc8c6e
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-07-15 22:06:46 +0300

    feat: configuration for redis connection pooling

---
+++ lib/go/redis.go
@@ -0,0 +1,21 @@
+package go
+
+import ("github.com/redis/go-redis/v9")
+
+
+// Configuration for Redis connection
+type RedisConfig struct {
+	URL            string
+	PoolSize       int
+	ConnectTimeout time.Duration
+	MaxRetries     int
+}
+
+func DefaultRedisConfig() RedisConfig {
+	return RedisConfig{
+		URL:            "redis://127.0.0.1:6379",
+		PoolSize:       10,
+		ConnectTimeout: 5 * time.Second,
+		MaxRetries:     3,
+	}
+}
